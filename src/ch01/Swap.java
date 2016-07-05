d60b7c481be223ca1b10073fcf2b16848cdc3186
SKY <SKY@SKY-PC>
2016-07-05 09:38:31 +0900

    1st commit

--- src/ch01/Swap.java
+++ src/ch01/Swap.java
@@ -15,45 +15,38 @@ public class Swap {
 		System.out.println("n2: " + n2);
 		System.out.println("========================");
 
-		int a1 = 20;
+		int a1 = 120;
 		int a2 = 70;
-		int a3 = 50;
+		int a3 = 150;
 
 		if (a1 >= a2) {
 			if (a1 >= a3) {
 				System.out.println("최대값: " + a1);
 			} else {
-				if (a2 >= a3) {
-					System.out.println("최대값: " + a2);
-				} else if (a1 >= a3) {
-					System.out.println("최대값: " + a1);
-				} else {
 					System.out.println("최대값: " + a3);
 				}
 			}
-		} else {
+		 else {
 			if (a2 >= a3) {
 				System.out.println("최대값: " + a2);
-			} else if (a1 >= a3) {
-				System.out.println("최대값: " + a1);
 			} else {
 				System.out.println("최대값: " + a3);
 			}
 		}
 
-		// int a[] = { 20, 10, 50 };
-		//
-		// for (int i = 0; i < a.length; i++) {
-		// for (int j = 0; j < a.length; j++) {
-		//
-		// if (a[i] < a[j]) {
-		// int temp = a[i];
-		// a[i] = a[j];
-		// a[j] = temp;
-		// }
-		// }
-		// }
-		// System.out.println("최대값: " + a[2]);
+		 int a[] = { 20, 10, 50 };
+		
+		 for (int i = 0; i < a.length; i++) {
+		 for (int j = 0; j < a.length; j++) {
+		
+		 if (a[i] < a[j]) {
+		 int temp = a[i];
+		 a[i] = a[j];
+		 a[j] = temp;
+		 }
+		 }
+		 }
+		 System.out.println("최대값: " + a[a.length-1]);
 
 	}
 }
